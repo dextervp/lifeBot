f73e17fd4122211716d84135de89d6eb2196aaf0
cognocoder <vikeshpyati@gmail.com>
2018-05-29 23:18:55 +0530

    adding respone to bot Call

--- mainApp.py
+++ mainApp.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import Flask,redirect
 
 app = Flask(__name__)
-
+redUrl = 'https://api.telegram.org/bot599314438:AAFLNLeqeIvyJtbqACQj2s4XUPbY9PDHn_c/sendMessage?chat_id=420358659&text=This is a LifeBot'
 
 @app.route('/botCall')
 def hello():
-    return 'Hello, bot!'
+    return redirect(redUrl)
 
 
 def main():

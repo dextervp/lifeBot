e038a48737310e4fb07f91cd856165603dedacb2
Vikesh Pyati <dextervp@users.noreply.github.com>
2018-05-30 11:27:29 +0530

    Update mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import Flask,redirect
 
 app = Flask(__name__)
-redUrl = 'https://api.telegram.org/bot599314438:AAFLNLeqeIvyJtbqACQj2s4XUPbY9PDHn_c/sendMessage?chat_id=420358659&text=This is a LifeBot'
+redUrl = 'https://api.telegram.org/bot<token>/sendMessage?chat_id=420358659&text=This is a LifeBot'
 
 @app.route('/botCall')
 def hello():
